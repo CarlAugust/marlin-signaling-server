e5315cb1f8014545bc19b37467e5a1f0fc4c3dc7
DumbAugust <carlaugust.sponberg@gmail.com>
2026-07-22 00:14:31 +0200

    First Commit, just the setup in Intellij

---
+++ src/test/java/com/example/filemarlin/FileMarlinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.filemarlin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileMarlinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
