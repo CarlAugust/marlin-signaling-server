245d0e895ebf1ad0e52e11f97622dca6da8cae0a
DumbAugust <carlaugust.sponberg@gmail.com>
2026-07-23 16:27:41 +0200

    Feat: Relay functionality. it has no security as off yet, now we just need a way to discover ids through API rest api probobly.

--- src/main/java/com/example/filemarlin/SignalHandler.java
+++ src/main/java/com/example/filemarlin/SignalHandler.java
@@ -1,35 +1,59 @@
 package com.example.filemarlin;
 
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
-import java.util.Map;
-import java.util.UUID;
-import java.util.concurrent.ConcurrentHashMap;
+import tools.jackson.databind.JsonNode;
+import tools.jackson.databind.ObjectMapper;
 
 public class SignalHandler extends TextWebSocketHandler {
 
-    private final Map<String, WebSocketSession> userSession = new ConcurrentHashMap<>();
+    private final Map<String, WebSocketSession> clientSessions = new ConcurrentHashMap<>();
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    public ArrayList<String> getConnectedClientIds() {
+        return new ArrayList<>(clientSessions.keySet());
+    }
 
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
         UUID uuid = UUID.randomUUID();
         String id = uuid.toString();
 
-        userSession.put(id, session);
+        clientSessions.put(id, session);
         session.getAttributes().put("id", id);
+
+        System.out.println("Client connected");
     }
 
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
-        super.handleTextMessage(session, message);
+        JsonNode jsonNode = objectMapper.readTree(message.getPayload());
+
+        // TODO: Error back to client
+        try {
+            String targetId = jsonNode.get("targetId").asText();
+            WebSocketSession targetSession = clientSessions.get(targetId);
+            if (targetSession != null && targetSession.isOpen()) {
+                targetSession.sendMessage(message);
+            }
+        } catch (NullPointerException e) {
+            System.err.println("No target ID provided");
+        }
+
+
     }
 
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
-        String id = (String) session.getAttributes().get("key");
-        userSession.remove(id);
+        String id = (String) session.getAttributes().get("id");
+        clientSessions.remove(id);
     }
 }
