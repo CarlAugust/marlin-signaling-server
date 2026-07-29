4f0b58c25316add1341346f1d829e00c612d1394
DumbAugust <carlaugust.sponberg@gmail.com>
2026-07-29 23:07:28 +0200

    Feat: UserRepository

--- src/main/java/com/example/filemarlin/repository/UserRepository.java
+++ src/main/java/com/example/filemarlin/repository/UserRepository.java
@@ -1,7 +1,29 @@
 package com.example.filemarlin.repository;
 
-import com.example.filemarlin.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
 
+import com.example.filemarlin.entity.User;
+
+@Repository
 public class UserRepository {
+    private final JdbcTemplate jdbcTemplate;
+
+    public UserRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    private final RowMapper<User> userRowMapper = (results, rowNum) -> {
+        User user = new User();
+        user.setId(results.getLong("id"));
+        user.setUsername(results.getString("username"));
+        user.setPassword(results.getString("password"));
+        return user;
+    };
+
+    public int save(User user) {
+        String sql = "INSERT INTO users (username, password) VALUES (?, ?)";
+        return jdbcTemplate.update(sql, user.getUsername(), user.getPassword());
+    }
 }
