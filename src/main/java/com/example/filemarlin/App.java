67b73b585bb8b5c2c50c59249e397fc4872d4dbb
DumbAugust <carlaugust.sponberg@gmail.com>
2026-07-30 14:55:32 +0200

    Test: Simply inserting a user to test if it works.

--- src/main/java/com/example/filemarlin/App.java
+++ src/main/java/com/example/filemarlin/App.java
@@ -1,13 +1,30 @@
 package com.example.filemarlin;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.example.filemarlin.repository.UserRepository;
+import com.example.filemarlin.service.UserService;
+
 
 @SpringBootApplication
 public class App {
 
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner initDatabase(UserRepository userRepository, UserService userService, PasswordEncoder passwordEncoder) {
+        return args -> {
+            // Check if test user already exists to avoid duplicate entries
+            if (userRepository.findByUsername("user").isEmpty()) {
+                userService.registerUser("user", "password");
+            }
+        };
     }
     
 }
