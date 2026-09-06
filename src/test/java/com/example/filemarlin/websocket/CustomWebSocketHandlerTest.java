07112f2567ed17f5866563a18a1a8313296e1d0c
DumbAugust <carlaugust.sponberg@gmail.com>
2026-09-06 18:25:50 +0200

    Remove some unused imports

--- src/test/java/com/example/filemarlin/websocket/CustomWebSocketHandlerTest.java
+++ src/test/java/com/example/filemarlin/websocket/CustomWebSocketHandlerTest.java
@@ -5,12 +5,9 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
-import org.springframework.web.socket.client.standard.StandardWebSocketClient;
 import tools.jackson.databind.JsonNode;
 import tools.jackson.databind.ObjectMapper;
 
