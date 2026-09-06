dd376b95a185203456f45899c949096d43c7ca46
DumbAugust <carlaugust.sponberg@gmail.com>
2026-09-06 17:03:10 +0200

    Feat: SignalHandler to CustomWebSocketHandler.
    connectedSessionsOnUser map and hopefully thread safe interactions with it.  And the simple get-clients message.

--- src/main/java/com/example/filemarlin/websocket/CustomWebSocketHandler.java
+++ src/main/java/com/example/filemarlin/websocket/CustomWebSocketHandler.java
@@ -1,9 +1,6 @@
 package com.example.filemarlin.websocket;
 
-import java.util.ArrayList;
-import java.util.Map;
-import java.util.Objects;
-import java.util.UUID;
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 
 import org.jspecify.annotations.NonNull;
@@ -15,7 +12,7 @@ import org.springframework.web.socket.handler.TextWebSocketHandler;
 import tools.jackson.databind.JsonNode;
 import tools.jackson.databind.ObjectMapper;
 
-public class SignalHandler extends TextWebSocketHandler {
+public class CustomWebSocketHandler extends TextWebSocketHandler {
 
     /*
     Note to self
@@ -23,12 +20,9 @@ public class SignalHandler extends TextWebSocketHandler {
     something that stores all clients with the same username that is connected.
     Such that its possible to see all of them or something.
      */
-    private final Map<String, WebSocketSession> clientSessions = new ConcurrentHashMap<>();
+    private final Map<String, WebSocketSession> connectedSessions = new ConcurrentHashMap<>();
+    private final Map<String, Set<String>> connectedSessionsOnUser = new ConcurrentHashMap<>();
     private final ObjectMapper objectMapper = new ObjectMapper();
-
-    public ArrayList<String> getConnectedClientIds() {
-        return new ArrayList<>(clientSessions.keySet());
-    }
 
     @Override
     public void afterConnectionEstablished(@NonNull WebSocketSession session) throws Exception {
@@ -36,26 +30,54 @@ public class SignalHandler extends TextWebSocketHandler {
         UUID uuid = UUID.randomUUID();
         String sessionId = uuid.toString();
 
-        clientSessions.put(sessionId, session);
+        connectedSessions.put(sessionId, session);
         session.getAttributes().put("sessionId", sessionId);
 
         String username = (String) Objects.requireNonNull(session.getPrincipal()).getName();
         session.getAttributes().put("username", username);
+
+        connectedSessionsOnUser
+                .computeIfAbsent(username, k -> ConcurrentHashMap.newKeySet())
+                .add(sessionId);
     }
 
     @Override
     protected void handleTextMessage(@NonNull WebSocketSession session, TextMessage message) throws Exception {
 
-        JsonNode jsonNode = objectMapper.readTree(message.getPayload());
-
-        // TODO: Error back to client
         try {
             /*
                 Grab target and data
                 Then validate data as expected type for target
             */
-            String targetId = jsonNode.get("targetId").asString();
-            JsonNode data = jsonNode.get("data");
+            JsonNode jsonNode = objectMapper.readTree(message.getPayload());
+            String messageType = jsonNode.get("type").asString();
+            Map<String, Object> response = Map.of();
+
+            switch (messageType) {
+
+                case "webrtc-signal":
+
+                    break;
+                case "get-clients":
+
+                    String username = (String) session.getAttributes().get("username");
+                    String[] sessions = (String[]) connectedSessionsOnUser.get(username).toArray();
+
+                    response = Map.of(
+                            "type", "get-clients",
+                            "clients", sessions
+                    );
+                    
+                    break;
+                default:
+                    response = Map.of(
+                            "type", "error",
+                            "message", "invalid type"
+                    );
+            }
+
+            String jsonPayload = objectMapper.writeValueAsString(response);
+            session.sendMessage(new TextMessage(jsonPayload));
 
 
         } catch (NullPointerException e) {
@@ -71,6 +93,11 @@ public class SignalHandler extends TextWebSocketHandler {
         String username = (String) session.getAttributes().get("username");
 
 
-        clientSessions.remove(sessionId);
+        connectedSessions.remove(sessionId);
+
+        connectedSessionsOnUser.computeIfPresent(username, (k, sessions) -> {
+            sessions.remove(sessionId);
+            return sessions.isEmpty() ? null : sessions;
+        });
     }
 }
